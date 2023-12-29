9d1a432d674ec8606f5b10e705980a2ef730b732
vharshitha1295 <harshithav1295@gmail.com>
2023-12-28 23:01:50 -0500

    My First SauceLab Automation

---
+++ src/test/java/SauaceLab/SauceLabAutomation/AppTest.java
@@ -0,0 +1,13 @@
+package SauaceLab.SauceLabAutomation;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+    public void shouldAnswerWithTrue()
+    {
+    }
+}
---
+++ src/test/java/SauaceLab/SauceLabAutomation/IOSRealDeviceTest.java
@@ -0,0 +1,31 @@
+package SauaceLab.SauceLabAutomation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.ios.IOSDriver;
+
+public class IOSRealDeviceTest {
+  @Test
+  public void realDeviceIOS() throws MalformedURLException, InterruptedException {
+	  MutableCapabilities caps = new MutableCapabilities();
+	  caps.setCapability("platformName", "iOS");
+	  caps.setCapability("appium:app", "storage:filename=iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.7.1.ipa");  // The filename of the mobile app
+	  caps.setCapability("appium:deviceName", "iPhone.*");
+	  caps.setCapability("appium:automationName", "XCUITest");
+	  MutableCapabilities sauceOptions = new MutableCapabilities();
+	  sauceOptions.setCapability("username", "oauth-vharshitha1295-9157b");
+	  sauceOptions.setCapability("accessKey", "ed42af79-cb08-4dad-8fd1-13e506581628");
+	  sauceOptions.setCapability("build", "build-001");
+	  sauceOptions.setCapability("name", "RealDeviceIOSTest");
+	  caps.setCapability("sauce:options", sauceOptions);
+
+	  URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+	  IOSDriver driver = new IOSDriver(url, caps);
+	  Thread.sleep(5000);
+	  driver.quit();
+  }
+}
